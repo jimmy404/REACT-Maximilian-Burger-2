9ef1a666761155f321553f3be2ed2396a99e363f
Jimmy <bruno_pier@hotmail.com>
2020-01-21 22:44:37 -0300

    Adding imports&wrapp with Provider - index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'; 
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const app = (
+<Provider>
     <BrowserRouter>
         <App />
     </BrowserRouter>
+</Provider>
 );
 
 ReactDOM.render( app, document.getElementById( 'root' ) );
