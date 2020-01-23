5cb3f07a5109425616f4ca532836b8394fa489ce
Jimmy <bruno_pier@hotmail.com>
2020-01-22 23:56:21 -0300

    Adding code to -Working on Order Actions- 1 bug to fix

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -92,8 +92,7 @@ class ContactData extends Component {
                 valid: true
             }
         },
-        formIsvalid: false,
-        loading: false
+        formIsvalid: false
     }
 
     orderHandler = ( event ) => {
@@ -169,7 +168,7 @@ class ContactData extends Component {
                 <Button btnType="Success" disabled={!this.state.formIsvalid}>ORDER</Button>
             </form>
         );
-        if ( this.state.loading ) {
+        if ( this.props.loading ) {
             form = <Spinner />;
         }
         return (
@@ -184,12 +183,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     }
-}
-
-const mapDispatchToProps = dispatch => {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart())
 };
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+const mapDispatchToProps = dispatch => {
+    return {
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
